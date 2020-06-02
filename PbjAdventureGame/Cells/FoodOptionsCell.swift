93d5be78d7659e66f44b2c43fe80b4ad48e42988
yazzy4 <yazburrell@pursuit.org>
2020-06-02 14:32:31 -0400

    Continuing UI rebuild

--- PbjAdventureGame/Cells/FoodOptionsCell.swift
+++ PbjAdventureGame/Cells/FoodOptionsCell.swift
@@ -19,8 +19,6 @@ class FoodOptionsCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-       
     }
 
 }
